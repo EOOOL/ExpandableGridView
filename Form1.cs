a557616f3fc7ca7580342963fc666221fa2daaa4
dzh <570904267@qq.com>
2024-06-19 15:53:36 +0800

    Initial

---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace ExpandableGridView
+{
+    
+    public partial class Form1 : Form
+    {
+        private DataTable dataTable1;
+        int getNum = 0;
+        List<DataRow> storeDisplayedRow = new List<DataRow>();
+        HierarchicalDataTable rootDt = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGridView();
+        }
+
+        private void InitializeGridView()
+        {
+            dataTable1 = new DataTable();
+            dataGridView1.AutoGenerateColumns = false;
+            typeof(Control)
+                .GetProperty("DoubleBuffered", BindingFlags.NonPublic | BindingFlags.Instance)
+                .SetValue(dataGridView1, true, null);
+            var cycleData = GetDataTable(60);
+            rootDt = new HierarchicalDataTable(cycleData);
+            var tmp1 = GetDataTable(3);
+            var tmp2 = GetDataTable(4);
+            rootDt.AddChild(tmp1, 2);
+            rootDt.AddChild(tmp2, 3);
+            for (int i = 0; i < cycleData.Rows.Count; i++)
+            {
+                //cycleData.Rows[i][0] = $"[+]{cycleData.Rows[i][0]}";
+                DataTable cycleData1 = new DataTable();
+                if (i % 2 == 0)
+                    cycleData1 = GetDataTable(6);
+                //主循环n有数据：
+                if (cycleData1 != null && cycleData1.Rows.Count > 0)
+                {
+                    cycleData.Rows[i][0] = $"[+]{cycleData.Rows[i][0]}";
+                }
+                else {
+                    cycleData.Rows[i][0] = $"   {cycleData.Rows[i][0]}";
+                }
+            }
+
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.VirtualMode = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.CellValueNeeded += DataGridView1_CellValueNeeded;
+            dataGridView1.RowCount = rootDt.Rows.Count;
+        }
+
+        private void DataGridView1_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                if (rootDt.Rows.Count <= e.RowIndex || rootDt.Rows[e.RowIndex].Table.Columns.Count <= e.ColumnIndex)
+                    e.Value = string.Empty;
+                else
+                    e.Value = rootDt.Rows[e.RowIndex][e.ColumnIndex].ToString();
+
+            }
+        }
+
+        private DataTable GetDataTable(int count)
+        {
+            DataTable dt = new DataTable();
+            for (int i = 0; i < 35 + getNum; i++)
+            {
+                dt.Columns.Add($"Name{i}", typeof(string));
+            }
+            if (dataGridView1.Columns.Count < dt.Columns.Count)
+            {
+                for (int i = 0, k = dt.Columns.Count - dataGridView1.Columns.Count; i < k; i++)
+                {
+                    dataGridView1.Columns.Add(
+                        new DataGridViewTextBoxColumn
+                        {
+                            HeaderText = dt.Columns[i].ColumnName,
+                            Name = dt.Columns[i].ColumnName
+                        }
+                    );
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                dt.Rows.Add(
+                    $"{getNum}---{i}",
+                    $"Jane{i}",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John",
+                    "John"
+                );
+            }
+            getNum++;
+            return dt;
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex != 0 || e.RowIndex < 0)
+                return;
+            DataGridView dataGridView = sender as DataGridView;
+            string cellText =
+                dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value?.ToString() ?? "";
+
+            int plusPos = cellText.IndexOf("[+]");
+            if (plusPos != -1)
+            {
+                // 获取"[+]"字符串的大小（考虑字体）
+                Font cellFont = dataGridView1.DefaultCellStyle.Font;
+                SizeF plusTextSize = TextRenderer.MeasureText("[+]", cellFont);
+
+                // 计算"[+]"在单元格中的实际位置（X坐标）
+                var cellRect = dataGridView1.GetCellDisplayRectangle(
+                    e.ColumnIndex,
+                    e.RowIndex,
+                    false
+                );
+                cellRect.Offset(-2, 0);
+                int plusPosX =
+                    cellRect.X + TextRenderer.MeasureText(new string(' ', plusPos), cellFont).Width; // 考虑到空格的宽度
+
+                // 判断点击是否落在"[+]"上
+                bool isPlusClicked =
+                    e.X >= plusPosX && e.X <= plusPosX + (int)plusTextSize.Width - 10;  // TextRenderer.MeasureText 方法返回的宽度可能包含额外的边距或间距，因此与单纯的字符宽度存在一定差异（GPT）
+                                                                                        // 27寸显示器下测试，-10可以将范围限制在[+]内
+
+                if (isPlusClicked) 
+                {
+                    //查询数据并增加到树形结构中
+                    var tmp = GetDataTable(8);
+
+                
+                }
+            }
+
+            int minusPos = cellText.IndexOf("[-]");
+            if (minusPos != -1)
+            {
+                Font cellFont = dataGridView1.DefaultCellStyle.Font;
+                SizeF plusTextSize = TextRenderer.MeasureText("[-]", cellFont);
+
+                var cellRect = dataGridView1.GetCellDisplayRectangle(
+                    e.ColumnIndex,
+                    e.RowIndex,
+                    false
+                );
+                int plusPosX =
+                    cellRect.X
+                    + TextRenderer.MeasureText(new string(' ', minusPos), cellFont).Width;
+
+                bool isPlusClicked =
+                    e.X >= plusPosX && e.X <= plusPosX + (int)plusTextSize.Width - 10;
+
+                if (isPlusClicked)
+                {
+                    MessageBox.Show("Clicked on '-'!");
+                }
+            }
+        }
+    }
+}
---
+++ HierarchicalDataTable.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpandableGridView;
+
+namespace WindowsFormsApp1
+{
+    enum DataLevel : int
+    {
+        Cycle = 0,
+        Step = 1,
+        Record = 2
+    }
+
+    public enum ExpandStatus
+    {
+        NAN,
+        Expand,
+        Collapse
+    }
+
+    public class HierarchicalDataTable
+    {
+        private const string LEVELMARK = "LEVELMARK";
+        private List<DataRow> _rows;
+        private List<HierarchicalDataTable> _children;
+        public bool HasChildNode => _children.Count > 0;
+        public List<DataRow> Rows => _rows;
+        private HierarchicalDataTable _parent = null;
+        bool? _isExpanded = null; // null:没有子节点；false:有子节点但没展开;true:有子节点且已展开
+        private readonly int SPACENUM = 3;
+
+        public HierarchicalDataTable(DataTable srcDataTable)
+        {
+            _rows = srcDataTable.AsEnumerable().ToList();
+            _children = new List<HierarchicalDataTable>();
+            srcDataTable.ExtendedProperties[LEVELMARK] = 0;
+        }
+
+        private HierarchicalDataTable(DataTable srcDataTable, bool isSave) //子节点构造器，_rows为null，不记录数据，减少内存占用----->改为第1层子节点（Step）保留数据，最后一层不保留？
+        {
+            _rows = isSave ? srcDataTable.AsEnumerable().ToList() : null;
+            _children = new List<HierarchicalDataTable>();
+        }
+
+        public void AddChild(DataTable childData, int targetRowIndex, bool isSave=false)
+        {
+            HierarchicalDataTable rootDt = GetRootDt();
+            if (targetRowIndex < 0 || targetRowIndex >= rootDt._rows.Count)
+                throw new IndexOutOfRangeException("根节点行数<目标行数");
+
+            int level = Convert.ToInt32(rootDt.Rows[targetRowIndex].Table.ExtendedProperties[LEVELMARK]) + 1;
+            childData.ExtendedProperties[LEVELMARK] = level;
+
+            //创建子节点，设置其父节点
+            var child = new HierarchicalDataTable(childData, isSave);
+            child._parent = this;
+            child._parent._isExpanded = true;
+            foreach (DataRow row in childData.Rows)
+            {
+                row[0] = "".PadLeft(level * SPACENUM, ' ') + row[0].ToString();
+            }
+
+            //找到根节点，在根节点添加数据
+
+            rootDt._rows.InsertRange(targetRowIndex + 1, childData.AsEnumerable().ToList());
+            ;
+            rootDt._rows[targetRowIndex][0] = "[-]" + rootDt._rows[targetRowIndex][0].ToString(); //根节点对应行添加展开图标
+
+            _children.Add(child);
+        }
+
+        public void SetExpandStatus(int rowIndex, ExpandStatus state)
+        {
+            var rootDt = GetRootDt();
+            if (rowIndex >= rootDt._rows.Count)
+                throw new IndexOutOfRangeException("根节点行数不满足");
+            //获取当前rowIndex对应的datatable,得到level
+            int level = Convert.ToInt32(rootDt.Rows[rowIndex].Table.ExtendedProperties[LEVELMARK]);
+
+            switch (state)
+            {
+                case ExpandStatus.NAN:
+                    break;
+                case ExpandStatus.Expand:
+                    var rowStr = rootDt.Rows[rowIndex][0].ToString().Replace("[-]", "");
+                    rootDt.Rows[rowIndex][0] = $"[+]{rootDt.Rows[rowIndex][0]}";
+                    break;
+                case ExpandStatus.Collapse:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private HierarchicalDataTable GetRootDt()
+        {
+            HierarchicalDataTable rootDt = this;
+            while (rootDt._parent != null)
+            {
+                rootDt = this._parent;
+            }
+            return rootDt;
+        }
+
+        //private int GetLevel(int rowIndex)
+        //{
+        //    var rootDt = GetRootDt();
+        //    return Convert.ToInt32(rootDt.Rows[rowIndex].Table.TableName);
+        //}
+    }
+}
